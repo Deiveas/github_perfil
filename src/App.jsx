4da128a6b20fe50437d95ac50da837f79a1c80a9
Deive Silva <deiveas@gmail.com>
2024-04-22 12:03:55 -0300

    botões aplicados e estilizados, aplicado mensagem de erro de usuário inexistente

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,35 @@ import ReposList from "./components/ReposList";
 function App() {
   //const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState("");
+  const botaoSubmit = (evento) => {
+    evento.prevenDefault()
+  }
 
   return (
     <>
-      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+    <div onSubmit={botaoSubmit}>
+    <input
+        type="text"
+        onBlur={(e) => setNomeUsuario(e.target.value)}
+        placeholder="Digite o nome do perfil"
+      />
+      <button type="submit">Buscar Perfil</button>
+    </div>
+      <>
 
-      {nomeUsuario.length > 4 && (
-        <>
-          <Perfil nomeUsuario={nomeUsuario} />
-          <ReposList nomeUsuario={nomeUsuario} />
-        </>
-      )}
+        {nomeUsuario.length > 4 && (
+          <>
+            <Perfil nomeUsuario={nomeUsuario} />
+            <ReposList nomeUsuario={nomeUsuario} />
+          </>
+        )}
 
-      {/* {formularioEstaVisivel && (
-        <Formulario/>
-      )}
+        {/* {formularioEstaVisivel && (
+      <Formulario/>
+    )}
 
-      <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">Toggle form</button> */}
+    <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">Toggle form</button> */}
+      </>
     </>
   );
 }
--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -7,16 +7,29 @@ import styles from "./ReposList.module.css";
 const ReposList = ({ nomeUsuario }) => {
   const [repos, setRepos] = useState([]);
   const [estaCarregando, setEstaCarregando] = useState(true);
+  const [usuarioInexistente, setUsuarioInexistente] = useState(false); // Definindo o estado inicial para usuarioInexistente
 
   useEffect(() => {
     setEstaCarregando(true);
     fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("Usuário inexistente"); // Lançar um erro se a resposta não for ok
+        }
+      })
       .then((resJson) => {
         setTimeout(() => {
           setEstaCarregando(false);
           setRepos(resJson);
+          setUsuarioInexistente(false); // Resetar o estado de usuarioInexistente se a requisição for bem-sucedida
         }, 3000);
+      })
+      .catch((error) => {
+        console.error(error);
+        setEstaCarregando(false);
+        setUsuarioInexistente(true); // Definir usuarioInexistente como true se ocorrer um erro
       });
   }, [nomeUsuario]);
 
@@ -24,9 +37,10 @@ const ReposList = ({ nomeUsuario }) => {
     <div className="container">
       {estaCarregando ? (
         <h1>Carregando...</h1>
+      ) : usuarioInexistente ? (
+        <h1 className={`${styles.errorMessage} errorMessage`}>Usuário Inexistente !!! <br/> Digite novamente !!!</h1>
       ) : (
         <ul className={styles.list}>
-          {/* {repos.map(respositorio =>( */}
           {repos.map(({ id, name, language, html_url }) => (
             <li className={styles.listItem} key={id}>
               <div className={styles.itemName}>
